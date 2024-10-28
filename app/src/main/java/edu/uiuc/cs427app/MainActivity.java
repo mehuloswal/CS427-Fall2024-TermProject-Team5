da8b84f1ea24956871b31fcfa923fcfc2c9e20d3
RuipengHan <ruipenghan@gmail.com>
2024-10-28 16:22:53 -0500

    Implement fireabase user auth - login/logout/resgiter UI and Backend.

--- app/src/main/java/edu/uiuc/cs427app/MainActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/MainActivity.java
@@ -12,16 +12,51 @@ import androidx.navigation.ui.AppBarConfiguration;
 import edu.uiuc.cs427app.databinding.ActivityMainBinding;
 
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+
+    FirebaseAuth auth;
+    FirebaseUser user;
+    TextView userDetails;
+
+    Button logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth= FirebaseAuth.getInstance();
+        logoutBtn = findViewById(R.id.logout_btn);
+        userDetails = findViewById(R.id.user_details);
+
+        user = auth.getCurrentUser();
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // Displays user email
+            userDetails.setText(user.getEmail());
+        }
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         // Initializing the UI components
         // The list of locations should be customized per user (change the implementation so that
