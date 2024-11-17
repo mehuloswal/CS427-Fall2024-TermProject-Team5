efc1737eab038264f2d72ac4420fcbdab9aab22d
RuipengHan <ruipenghan@gmail.com>
2024-11-16 23:46:45 -0600

    Added key to local.properties.

--- app/src/main/java/edu/uiuc/cs427app/Config.java
+++ app/src/main/java/edu/uiuc/cs427app/Config.java
@@ -4,5 +4,5 @@ public class Config {
     // Set up the API URL from flask server (The one which has the IP address)
     public static final String API_URL = "http://10.0.2.2:5001/api/";
 
-    public static final String GEMINI_KEY = "AIzaSyBvbrRmmkRCaS8mC-QdeOpj6hU7iSGO07E";
+    public static final String GEMINI_KEY = BuildConfig.GEMINI_KEY;
 }
